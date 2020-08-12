98d206696218a0c6d7b9a464db7764f4ca06795f
Jens Reidel <jens@troet.org>
2020-08-12 16:10:13 +0200

    Implement models

--- veldpy/__init__.py
+++ veldpy/__init__.py
@@ -1,1 +1,7 @@
+from .models import *
+
+__title__ = "veldpy"
 __version__ = "0.1.0"
+__author__ = "Jens Reidel <adrian@travitia.xyz>"
+__license__ = "BSD-3-Clause"
+__copyright__ = "Copyright 2020 (c) Jens Reidel"
---
+++ veldpy/models.py
@@ -0,0 +1,74 @@
+"""
+Copyright (c) 2020, Jens Reidel
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+##############################################################################
+#
+
+
+#
+##############################################################################
+from __future__ import annotations
+
+# This is an implementation of the veldchat gateway models as described here:
+# https://github.com/velddev/node-chat-server/wiki/Models
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass
+class Embed:
+    author: Optional[EmbedAuthor]
+    title: Optional[str]
+    description: Optional[str]
+    color: Optional[int]
+    footer: Optional[str]
+    image_url: Optional[str]
+    thumbnail_url: Optional[str]
+
+
+@dataclass
+class EmbedAuthor:
+    value: str
+    icon_url: Optional[str]
+
+
+@dataclass
+class Message:
+    content: Optional[str]
+    embed: Optional[Embed]
+    mentions: List[int]
+    user: User
+
+
+@dataclass
+class User:
+    id: int
+    avatar_url: Optional[str]
+    name: str
+    bot: bool
